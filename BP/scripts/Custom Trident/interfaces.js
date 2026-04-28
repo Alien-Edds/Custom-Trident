d05b740a85abc026828c9372894b087da45545e1
Alien Edds <eddsallen@gmail.com>
2026-04-28 09:52:28 -0500

    changed to regolith format

--- BP/scripts/Custom Trident/interfaces.js
+++
@@ -1,6 +0,0 @@
-export var RiptideEnvironment;
-(function (RiptideEnvironment) {
-    RiptideEnvironment["Any"] = "any";
-    RiptideEnvironment["Water"] = "water";
-    RiptideEnvironment["OnFire"] = "onFire";
-})(RiptideEnvironment || (RiptideEnvironment = {}));
